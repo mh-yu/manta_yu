5f788e3795e3621fbedce6f6e111f8cd166e585c
mh-yu <yumohan01@gmail.com>
2026-04-06 08:50:00 -0600

    manta

--- benchmark/fabfile.py
+++ benchmark/fabfile.py
@@ -254,7 +254,7 @@ def cloudlab_remote(
     allow_cross_step_weak_edges=True,
     enable_fast_coin=True,
     enable_commit_recheck=True,
-    design_tag='manta-max_delay_50',
+    design_tag='manta-max_delay_100',
 ):
     ''' Run benchmarks on CloudLab '''
     allow_cross_step_weak_edges = _coerce_bool(allow_cross_step_weak_edges)
@@ -269,16 +269,16 @@ def cloudlab_remote(
         'rate': [60000],
         'tx_size': 512,
         'duration': 20,
-        'runs': 2,
+        'runs': 1,
     }
     node_params = {
         'header_size': 1_000,  # bytes
-        'max_header_delay': 50,  # ms
+        'max_header_delay': 200,  # ms
         'gc_depth': 50,  # rounds
         'sync_retry_delay': 1000,  # ms
         'sync_retry_nodes': 7,  # number of nodes
         'batch_size': 128_000,  # bytes
-        'max_batch_delay': 50,  # ms
+        'max_batch_delay': 200,  # ms
         'sigma': sigma,
         'kappa': kappa,
         'reference': reference,
