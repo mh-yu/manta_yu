bb28fac9213073601ccf5942daf1cc69e82c2b7b
michaelwcl <chenlinwu2-c@my.cityu.edu.hk>
2026-04-15 02:09:43 -0600

    manta

--- benchmark/fabfile.py
+++ benchmark/fabfile.py
@@ -332,7 +332,7 @@ def cloudlab_remote(
     # 'max_batch_delay': 35,  # ms
     node_params = {
         'header_size': 1_000,  # bytes
-        'max_header_delay': 50,  # ms
+        'max_header_delay': 120,  # ms
         'gc_depth': 50,  # rounds
         'sync_retry_delay': 1000,  # ms
         'sync_retry_nodes': 7,  # number of nodes
