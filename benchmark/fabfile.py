6689ee2863a72f986bb56afe75f8b3bea440e1e3
michaelwcl323 <chenlinwu2-c@my.cityu.edu.hk>
2026-04-20 22:12:03 +0000

    m

--- benchmark/fabfile.py
+++ benchmark/fabfile.py
@@ -149,10 +149,10 @@ def remote(ctx, debug=False):
         'rate_type': 'balanced',
         'design_tag': 'tusk-m_experiment3',
         'network_tag': 'geo',
-        'rate': [80000],
+        'rate': [80000,120000,160000,180000,200000],
         'tx_size': 512,
         'duration': 120,
-        'runs': 1,
+        'runs': 2,
     }
     node_params = {
         'header_size': 1_000,  # bytes
